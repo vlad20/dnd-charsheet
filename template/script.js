4e4e2d70479da15df413c66cf2471fe40e455952
vlad20 <grigorescu.vlad1@gmail.com>
2022-05-19 00:09:30 +0300

    added json download functionality for form input

--- template/script.js
+++ template/script.js
@@ -1,17 +1,16 @@
 $(document).ready(function () {
   $("#save").on("click", function () {
     $('input[type="text"]').each(function () {
-      var id = $(this).attr("id");
-      var value = $(this).val();
+      let id = $(this).attr("id");
+      let value = $(this).val();
       localStorage.setItem(id, value);
     });
   });
 
   $("#load").on("click", function () {
     $('input[type="text"]').each(function () {
-      var id = $(this).attr("id");
-      var value = localStorage.getItem(id);
-
+      let id = $(this).attr("id");
+      let value = localStorage.getItem(id);
       $(this).val(value);
     });
   });
@@ -21,4 +20,25 @@ $(document).ready(function () {
       $('form :input').val('');
     });
   });
+  
+  $("#json").on("click", function ( event ) {
+    let form = $('form').serializeArray();
+    console.log(form);
+    let jsonForm = JSON.stringify(form);
+    let fileBlob = new Blob([jsonForm], {
+      type: 'data:text/json;charset=utf-8'
+    });
+    let temp_anchor = document.createElement("a");
+    temp_anchor.style = "display: none";
+    let fileBlobURL = URL.createObjectURL(fileBlob); 
+    temp_anchor.href = fileBlobURL;
+    temp_anchor.download = "charsheet.json";
+    temp_anchor.click();
+    window.URL.revokeObjectURL(fileBlobURL);
+    temp_anchor.remove();
+    console.log(jsonForm);
+    event.preventDefault();
+  })
+
+  
 });
